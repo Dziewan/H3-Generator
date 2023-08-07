e1916b29b5d80e3382f1b2d751e74ada5bbeade8
mdziewanowski <m.dziewanowski@ambsoft.pl>
2023-08-07 17:04:26 +0200

    update

--- src/main/java/model/Match.java
+++ src/main/java/model/Match.java
@@ -1,14 +1,6 @@
 package model;
 
-public class Match {
-    private final Player firstPlayer;
-    private final Player secondPlayer;
-
-    public Match(Player firstPlayer, Player secondPlayer) {
-        this.firstPlayer = firstPlayer;
-        this.secondPlayer = secondPlayer;
-    }
-
+public record Match(Player firstPlayer, Player secondPlayer) {
     @Override
     public String toString() {
         return String.format("%s vs %s\n%s (%s) vs %s (%s)\n%s : %s\n%s : %s\n",
